2796387cfa3aa5ce863376ef567e5197502ebbca
DanielTkachenko <pdg.danila.98@mail.ru>
2023-07-21 22:46:48 +0700

    Added dataset preprocessing. Added extracting metrics from audiofiles. Added Binary classifier. Added API.

--- main.py
+++ main.py
@@ -1,14 +1,74 @@
+from fastapi import FastAPI, File
+import io
 import csv
 import os
 import librosa
 import numpy as np
-
 import pandas as pd
+from pandas import DataFrame as df
 import scipy
+from sklearn import preprocessing
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import train_test_split
+from sklearn import svm, neighbors
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+
+import my_dataset
+#creating dataset (need to execute once)
+#my_dataset.create_features_table("./data/features.csv")
+#my_dataset.feature_table_to_dataset("./data/dataset_1.csv", "./data/features.csv")
 
 N_MFCC = 20
 
-data = pd.read_csv('data/dataset.csv', encoding='cp1251')
-batch = data[:5]
-print(batch.columns)
-print(batch)
+import wandb
+wandb.login()
+run = wandb.init(
+    # Set the project where this run will be logged
+    project="SoundRecognition",
+    # Track hyperparameters and run metadata
+    config={
+        "metric": "accuracy",
+
+    })
+
+app = FastAPI(title="SoundRecognition")
+@app.post("files/upload-files")
+def upload_files(audiofile_1 : bytes = File(), audiofile_2 : bytes = File()):
+    y1, sr1 = librosa.load(io.BytesIO(audiofile_1), mono=True, duration=1)
+    y2, sr2 = librosa.load(io.BytesIO(audiofile_2), mono=True, duration=1)
+    #extracting of features
+    audio_1_features = np.asarray(my_dataset.get_features(y1, sr1)).reshape(-1, 1)
+    audio_2_features = np.asarray(my_dataset.get_features(y2, sr2)).reshape(-1, 1)
+    audio_features = [np.concatenate([audio_1_features, audio_2_features])]
+    #reading dataset to dataframe
+    data = pd.read_csv('data/dataset_1.csv', encoding='cp1251')
+    #extracting feature table (Х), and labels table (у)
+    X = data.iloc[:, 2:-1]
+    print(type(X))
+    X = preprocessing.StandardScaler().fit(X).transform(X)
+    print(type(X))
+    y = data["label"].values
+    # create and fit model
+    model = neighbors.KNeighborsClassifier(n_neighbors=3)
+    model.fit(X, y)
+    # get prediction of the model
+    y_pred = model.predict([])
+    # output result
+    #print("KNeighborsClassifier")
+    print("Train set Accuracy: ", metrics.accuracy_score(y, model.predict(X)))
+    similarity = round(100 - mean_absolute_percentage_error(audio_1_features, audio_2_features))
+    label = False
+    if y_pred[0] == 1:
+        label = True
+    return {'Access': label, 'Similarity': similarity}
+
+
+
+"""
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+model = svm.SVC(kernel='rbf', verbose=1)
+model.fit(X_train, y_train)
+"""
--- my_dataset.py
+++ my_dataset.py
@@ -8,14 +8,18 @@ import scipy
 
 N_MFCC = 20
 
+filepath = "./data/Sounds-dataset/Алексей/Вход-не распознано Алексей.wav"
+
+y, sr = librosa.load(filepath, mono=True)
+
 def find_match(file1, file2):
     for w in ["Вход", "Пицца", "Привет", "Собака", "Шкаф"]:
         if w in file1 and w in file2:
             return 1
     return 0
 
-def get_features(filepath):
-    y, sr = librosa.load(filepath, mono=True)
+
+def get_features(y, sr):
     features = []
     features.extend([np.mean(e) for e in librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)])  #mfcc_mean
     features.extend([np.std(e) for e in librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)])  # mfcc_std
@@ -26,36 +30,58 @@ def get_features(filepath):
     features.append(np.std(librosa.feature.spectral_rolloff(y=y, sr=sr).T,axis=0)[0])   # roloff_std
     return features
 
-def get_headers(files_n = 2):
+def get_headers(files_n = 2,mfcc_n=20, include_dirs = False):
     header = []
+    if include_dirs:
+        for n in range(files_n):
+            header.append(f'dir{n}')
     for n in range(files_n):
         header.append(f'fname{n}')
     for n in range(files_n):
-        header.extend([f'f{n}_mfcc_mean{i}' for i in range(1, 21)])
-        header.extend([f'f{n}_mfcc_std{i}' for i in range(1, 21)])
+        header.extend([f'f{n}_mfcc_mean{i}' for i in range(1, mfcc_n+1)])
+        header.extend([f'f{n}_mfcc_std{i}' for i in range(1, mfcc_n+1)])
         header.extend([f'f{n}_cent_mean', f'f{n}_cent_std', f'f{n}_cent_skew', f'f{n}_rolloff_mean', f'f{n}_rolloff_std'])
-    header.append("label")
     return header
 
+#execute once to create feature table
+def create_features_table(table_file_name):
+    with open(table_file_name, 'w', encoding='cp1251', newline ='') as file:
+        writer = csv.writer(file, delimiter=',')
+        h = get_headers(1, include_dirs=True)
+        writer.writerow(h)
+        for directory, _, filenames in os.walk("./data/Sounds-dataset/"):
+            for filename in filenames:
+                row = [directory]
+                row.append(filename)
+                y, sr = librosa.load(os.path.join(directory, filename), mono=True)
+                row.extend(get_features(y, sr))
+                writer.writerow(row)
+
+
 #execute once to create dataset
-def create_dataset():
-    with open('data/dataset.csv', 'w', encoding='cp1251', newline ='') as file:
+def feature_table_to_dataset(dataset_file_name, table_file_name):
+    with open(dataset_file_name, 'w', encoding='cp1251', newline ='') as file:
         writer = csv.writer(file, delimiter=',')
         h = get_headers()
+        h.append("label")
         writer.writerow(h)
-        for directory1, _, filenames1 in os.walk("./Sounds-dataset/"):
-            for filename1 in filenames1:
-                print(filename1)
-                for directory2, _, filenames2 in os.walk("./Sounds-dataset/"):
-                    for filename2 in filenames2:
-                        if directory1 == directory2 and filename1 == filename2:
-                            continue
-                        row = [filename1, filename2]
-                        row.extend(get_features(os.path.join(directory1, filename1)))
-                        row.extend(get_features(os.path.join(directory2, filename2)))
-                        if directory1 != directory2:
-                            label = 0
-                        elif directory1 == directory2:
-                            label = find_match(filename1, filename2)
-                        row.append(label)
-                        writer.writerow(row)
+        features = pd.read_csv(table_file_name, encoding='cp1251')
+        for f1 in features.values:
+            for f2 in features.values:
+                dir1, dir2 = f1[0], f2[0]
+                fname1, fname2 = f1[1], f2[1]
+                if dir1 != dir2:
+                    continue
+                if dir1 == dir2 and fname1 == fname2:
+                    continue
+                row = []
+                row.extend([fname1, fname2])
+                row.extend(f1[2:])
+                row.extend(f2[2:])
+                if dir1 != dir2:
+                    label = 0
+                elif dir1 == dir2:
+                    label = find_match(fname1, fname2)
+                row.append(label)
+                writer.writerow(row)
+
